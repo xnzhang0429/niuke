dd4c635e06846a76afe201f3b7d5bfab81f4dfc3
qinfendexiaoxiannv <xnzhang0429@163.com>
2018-05-20 16:47:32 +0800

    2018-5-20

--- src/offer/MergeLink.java
+++ src/offer/MergeLink.java
@@ -46,7 +46,7 @@ public class MergeLink {
             }
             else{
                 if(l3 == null){
-                    l3 = l4 = list1;
+                    l3 = l4 ;
                 }
                 else{
                     l4.next = list1;
